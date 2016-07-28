5797edd8985e8792cdb1c0d21afe7bbc98ef60b2
Neil Craig <neil.craig@bbc.co.uk>
2016-07-28 15:24:31 +0100

    v1.0.0

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,6 +4,10 @@
 Super simple Yeoman generrator to set up a Node NPM package
 */
 
+// Core deps
+const fs = require("fs");
+
+// Yeoman deps
 const generators = require("yeoman-generator");
 
 module.exports = generators.Base.extend(
@@ -45,6 +49,8 @@ TODO: split this up into a less horrendous chunk of crap
 
     // TODO: should this include the dir name too?
     this.GTNOpts.appLibFilename = this.GTNOpts.appName + "-lib.js";
+    this.GTNOpts.appConfigFilename = this.GTNOpts.appName + "-config.js";
+    this.GTNOpts.appConfigTemplateFilename = this.GTNOpts.appName + "-config-template.js";
 
     // app description
     this.option("description",
@@ -80,122 +86,130 @@ TODO: split this up into a less horrendous chunk of crap
 
 
     // License
+    // TODO: move this to config
+    // NOTE: this is the full list of current (July 2016), OSI approved SPDX licenses from https://spdx.org/licenses/. Commented license are not yet added to templates/licenses
+    const validLicenses =
+    [
+        // "0BSD",
+        // "AAL",
+        "AFL-1.1",
+        "AFL-1.2",
+        "AFL-2.0",
+        "AFL-2.1",
+        "AFL-3.0",
+        // "AGPL-3.0",
+        // "Apache-1.1",
+        // "Apache-2.0",
+        // "APL-1.0",
+        // "APSL-1.0",
+        // "APSL-1.1",
+        // "APSL-1.2",
+        // "APSL-2.0",
+        // "Artistic-1.0",
+        // "Artistic-1.0-cl8",
+        // "Artistic-1.0-Perl",
+        // "Artistic-2.0",
+        // "BSD-2-Clause",
+        // "BSD-3-Clause",
+        // "BSL-1.0",
+        // "CATOSL-1.1",
+        // "CDDL-1.0",
+        // "CECILL-2.1",
+        // "CNRI-Python",
+        // "CPAL-1.0",
+        // "CPL-1.0",
+        // "CUA-OPL-1.0",
+        // "ECL-1.0",
+        // "ECL-2.0",
+        // "EFL-1.0",
+        // "EFL-2.0",
+        // "Entessa",
+        // "EPL-1.0",
+        // "EUDatagrid",
+        // "EUPL-1.1",
+        // "Fair",
+        // "Frameworx-1.0",
+        "GPL-2.0",
+        "GPL-3.0",
+        // "HPND",
+        // "Intel",
+        // "IPA",
+        // "IPL-1.0",
+        // "ISC",
+        // "LGPL-2.0",
+        // "LGPL-2.1",
+        // "LGPL-3.0",
+        // "LiLiQ-P-1.1",
+        // "LiLiQ-R-1.1",
+        // "LiLiQ-Rplus-1.1",
+        // "LPL-1.0",
+        // "LPL-1.02",
+        // "LPPL-1.3c",
+        // "MirOS",
+        "MIT",
+        // "Motosoto",
+        // "MPL-1.0",
+        // "MPL-1.1",
+        "MPL-2.0",
+        // "MPL-2.0-no-copyleft-exception",
+        // "MS-PL",
+        // "MS-RL",
+        // "Multics",
+        // "NASA-1.3",
+        // "Naumen",
+        // "NCSA",
+        // "NGPL",
+        // "Nokia",
+        // "NPOSL-3.0",
+        // "NTP",
+        // "OCLC-2.0",
+        // "OFL-1.1",
+        // "OGTSL",
+        // "OSET-PL-2.1",
+        // "OSL-1.0",
+        // "OSL-2.0",
+        // "OSL-2.1",
+        // "OSL-3.0",
+        // "PHP-3.0",
+        // "PostgreSQL",
+        // "Python-2.0",
+        // "QPL-1.0",
+        // "RPL-1.1",
+        // "RPL-1.5",
+        // "RPSL-1.0",
+        // "RSCPL",
+        // "SimPL-2.0",
+        // "SISSL",
+        // "Sleepycat",
+        // "SPL-1.0",
+        // "UPL-1.0",
+        // "VSL-1.0",
+        // "W3C",
+        // "Watcom-1.0",
+        // "Xnet",
+        // "Zlib",
+        // "ZPL-2.0"
+    ];
+
     // TODO: Add an opt for license - default to MIT, git.name() ?
     this.option("license",
     {
+        desc: "Specify the software license to include in package.json and the license.md file. Valid license are: " + validLicenses.toString("utf8"),
         type: String,
         alias: "l",
         optional: true,
         defaults: "MIT"
     });
 
-    // TODO: move this to config
-    // NOTE: this is the full list of current (July 2016), OSI approved SPDX licenses from https://spdx.org/licenses/
-    const validLicenses =
-    [
-        "0BSD",
-        "AAL",
-        "AFL-1.1",
-        "AFL-1.2",
-        "AFL-2.0",
-        "AFL-2.1",
-        "AFL-3.0",
-        "AGPL-3.0",
-        "Apache-1.1",
-        "Apache-2.0",
-        "APL-1.0",
-        "APSL-1.0",
-        "APSL-1.1",
-        "APSL-1.2",
-        "APSL-2.0",
-        "Artistic-1.0",
-        "Artistic-1.0-cl8",
-        "Artistic-1.0-Perl",
-        "Artistic-2.0",
-        "BSD-2-Clause",
-        "BSD-3-Clause",
-        "BSL-1.0",
-        "CATOSL-1.1",
-        "CDDL-1.0",
-        "CECILL-2.1",
-        "CNRI-Python",
-        "CPAL-1.0",
-        "CPL-1.0",
-        "CUA-OPL-1.0",
-        "ECL-1.0",
-        "ECL-2.0",
-        "EFL-1.0",
-        "EFL-2.0",
-        "Entessa",
-        "EPL-1.0",
-        "EUDatagrid",
-        "EUPL-1.1",
-        "Fair",
-        "Frameworx-1.0",
-        "GPL-2.0",
-        "GPL-3.0",
-        "HPND",
-        "Intel",
-        "IPA",
-        "IPL-1.0",
-        "ISC",
-        "LGPL-2.0",
-        "LGPL-2.1",
-        "LGPL-3.0",
-        "LiLiQ-P-1.1",
-        "LiLiQ-R-1.1",
-        "LiLiQ-Rplus-1.1",
-        "LPL-1.0",
-        "LPL-1.02",
-        "LPPL-1.3c",
-        "MirOS",
-        "MIT",
-        "Motosoto",
-        "MPL-1.0",
-        "MPL-1.1",
-        "MPL-2.0",
-        "MPL-2.0-no-copyleft-exception",
-        "MS-PL",
-        "MS-RL",
-        "Multics",
-        "NASA-1.3",
-        "Naumen",
-        "NCSA",
-        "NGPL",
-        "Nokia",
-        "NPOSL-3.0",
-        "NTP",
-        "OCLC-2.0",
-        "OFL-1.1",
-        "OGTSL",
-        "OSET-PL-2.1",
-        "OSL-1.0",
-        "OSL-2.0",
-        "OSL-2.1",
-        "OSL-3.0",
-        "PHP-3.0",
-        "PostgreSQL",
-        "Python-2.0",
-        "QPL-1.0",
-        "RPL-1.1",
-        "RPL-1.5",
-        "RPSL-1.0",
-        "RSCPL",
-        "SimPL-2.0",
-        "SISSL",
-        "Sleepycat",
-        "SPL-1.0",
-        "UPL-1.0",
-        "VSL-1.0",
-        "W3C",
-        "Watcom-1.0",
-        "Xnet",
-        "Zlib",
-        "ZPL-2.0"
-    ];
-
-    // TODO: Make this less ugly
-    this.GTNOpts.license = validLicenses.indexOf(this.options.license) >= 0 ? this.options.license : "MIT";
+    if(validLicenses.indexOf(this.options.license) >= 0)
+    {
+        this.GTNOpts.license = this.options.license;
+    }
+    else
+    {
+        console.error("Please specify a supported license type, one of: " + validLicenses.toString("utf8"));
+        process.exit(1);
+    }
 
 
     // TODO: add license files
@@ -221,6 +235,11 @@ TODO: split this up into a less horrendous chunk of crap
     this.template(this.templatePath("node/app-lib.js"), "lib/" + this.GTNOpts.appLibFilename);
 
     // config dir
+    this.template(this.templatePath("node/config/app-config.js"), "lib/" + this.GTNOpts.appConfigFilename);
+    this.template(this.templatePath("node/config/app-config.js"), "lib/" + this.GTNOpts.appConfigTemplateFilename);
+
+    // postinstall script
+    this.template(this.templatePath("node/scripts/post-install"), "scripts/post-install.js");
 
     // .gitignore
     this.template(this.templatePath("source-control/.gitignore"), ".gitignore");
--- generators/app/templates/node/index.js
+++ generators/app/templates/node/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // TODO: have a specific var for the lib filename and use that for consistency
+const config = require("config/<%= GTNOpts.appConfigFilename %>");
 const lib = require("lib/<%= GTNOpts.appLibFilename %>");
 
 // TODO: config, likely deps, etc...
